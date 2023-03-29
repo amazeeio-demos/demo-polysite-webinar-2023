4d26cfba47cd0df5a6d36462daf0ec2fd8f8db6d
Bryan Gruneberg <hello@bryangruneberg.com>
2023-03-29 14:44:30 -0600

    Block placement and styling

--- web/modules/custom/custom_module/src/Plugin/Block/AioBlock.php
+++ web/modules/custom/custom_module/src/Plugin/Block/AioBlock.php
@@ -24,6 +24,15 @@ class AioBlock extends BlockBase {
   	'lo' => 'lo.png',
   ];
 
+  protected $countries = [
+  	'de' => 'Germany',
+  	'ch' => 'Switzerland',
+  	'us' => 'USA',
+  	'fi' => 'Finland',
+  	'eu' => 'Europe',
+  	'lo' => 'Local',
+  ];
+
   protected $logos = [
   	'de3' => 'aws.png',
   	'fi2' => 'gcp.png',
@@ -31,6 +40,15 @@ class AioBlock extends BlockBase {
   	'us3' => 'gcp.png',
   	'ch4' => 'gcp.png',
   	'dkr' => 'dkr.png',
+  ];
+
+  protected $providers = [
+  	'de3' => 'AWS',
+  	'fi2' => 'GCP',
+  	'us2' => 'AWS',
+  	'us3' => 'GCP',
+  	'ch4' => 'GCP',
+  	'dkr' => 'Docker',
   ];
 
   /**
@@ -47,17 +65,22 @@ class AioBlock extends BlockBase {
     $flagPath = \Drupal::service('extension.list.module')->getPath('custom_module') . "/flags/";
 
     $flag = "";
-    if(preg_match("/^(\w\w)\d\..amazee\.io$/", $cluster, $MAT)) {
-	$flag = '<img class="custom-module-flag" src="/' . $flagPath . "/" . $this->flags[$MAT[1]] . '" />';
+    $region = "Local";
+    if(preg_match("/^(\w\w)\d.amazee.io$/", $cluster, $MAT)) {
+	    $flag = '<img class="custom-module-flag" src="/' . $flagPath . "/" . $this->flags[$MAT[1]] . '" />';
+	    $region = $this->countries[$MAT[1]];
     } else if($cluster == "local") {
 	$flag = '<img class="custom-module-flag" src="/' . $flagPath . "/" . $this->flags['lo'] . '" />';
     }
 
     $logo = "";
-    if(preg_match("/^(\w\w\d)\..amazee\.io$/", $cluster, $MAT)) {
+    $infra = "";
+    if(preg_match("/^(\w\w\d).amazee.io$/", $cluster, $MAT)) {
 	$logo = '<img class="custom-module-logo" src="/' . $logoPath . "/" . $this->logos[$MAT[1]] . '" />';
+	$infra = $this->providers[$MAT[1]];
     } else if($cluster == "local") {
 	$logo = '<img class="custom-module-logo" src="/' . $logoPath . "/" . $this->logos['dkr'] . '" />';
+	$infra = "Docker";
     }
 
     $aio = '<img class="custom-module-aio-logo" src="/' . $logoPath . '/amazee-io-mirantis.svg" />';
@@ -65,7 +88,8 @@ class AioBlock extends BlockBase {
 	    '#attached'=> ['library' => ['custom_module/custom_module']],
 	    '#markup' => '<div class="aio-info">' . 
 	    	"<span class='aio-info-logo'>$aio</span>" . 
-	    	"<span class='aio-info-part'><span class='aio-info-part-title'>Cluster:</span> ".$cluster." $flag $logo</span>" .
+	    	"<span class='aio-info-part'>$logo ($infra)</span>" .
+	    	"<span class='aio-info-part'>$flag ($region)</span>" .
 	    	"<span class='aio-info-part'><span class='aio-info-part-title'>Project:</span> ".$project."</span>" . 
 		"<span class='aio-info-part'><span class='aio-info-part-title'>Environment:</span> ".$environment." (".$environment_type.")</span>" . 
 		"<span class='aio-info-part'><span class='aio-info-part-title'>AIO World Version:</span> ".$version."</span>" . 
